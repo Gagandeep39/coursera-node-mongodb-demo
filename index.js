9f117b50d6e7b502b13eef08ced49910708f00a5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-08 05:34:52 +0000

    Switched to promises

--- index.js
+++ index.js
@@ -14,65 +14,106 @@ require('dotenv').config();
 const url = `mongodb://${process.env.HOST_NAME || 'localhost'}:27017/`;
 const dbname = 'gagan';
 
-MongoClient.connect(url, (err, client) => {
-  assert.equal(err, null);
+MongoClient.connect(url)
+  .then((client) => {
+    console.log('Connected correctly to server');
+    const db = client.db(dbname);
 
-  console.log('Connected correctly to server');
+    dboper
+      .insertDocument(db, { name: 'Vadonut', description: 'Test' }, 'dishes')
+      .then((result) => {
+        console.log('Insert Document:\n', result.ops);
 
-  const db = client.db(dbname);
-  const collection = db.collection('dishes');
-  collection.insertOne(
-    { name: 'pasta', description: 'Cheese Pasta' },
-    (err, result) => {
-      assert.equal(err, null);
-
-      console.log('After Insert:\n');
-      console.log(result.ops);
-
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-
-        console.log('Found:\n');
-        console.log(docs);
-
-        db.dropCollection('dishes', (err, result) => {
-          assert.equal(err, null);
-        });
-      });
-    }
-  );
-
-  // Running operations of other file
-  dboper.insertDocument(
-    db,
-    { name: 'Pizza', description: 'Test' },
-    'dishes',
-    (result) => {
-      console.log('Insert Document:\n', result.ops);
-
-      dboper.findDocuments(db, 'dishes', (docs) => {
+        return dboper.findDocuments(db, 'dishes');
+      })
+      .then((docs) => {
         console.log('Found Documents:\n', docs);
 
-        dboper.updateDocument(
+        return dboper.updateDocument(
           db,
-          { name: 'Pizza' },
+          { name: 'Vadonut' },
           { description: 'Updated Test' },
-          'dishes',
-          (result) => {
-            console.log('Updated Document:\n', result.result);
-
-            dboper.findDocuments(db, 'dishes', (docs) => {
-              console.log('Found Updated Documents:\n', docs);
-
-              db.dropCollection('dishes', (result) => {
-                console.log('Dropped Collection: ', result);
-
-                client.close();
-              });
-            });
-          }
+          'dishes'
         );
-      });
-    }
-  );
-});
+      })
+      .then((result) => {
+        console.log('Updated Document:\n', result.result);
+
+        return dboper.findDocuments(db, 'dishes');
+      })
+      .then((docs) => {
+        console.log('Found Updated Documents:\n', docs);
+
+        return db.dropCollection('dishes');
+      })
+      .then((result) => {
+        console.log('Dropped Collection: ', result);
+
+        return client.close();
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
+
+// MongoClient.connect(url, (err, client) => {
+//   assert.equal(err, null);
+
+//   console.log('Connected correctly to server');
+
+//   const db = client.db(dbname);
+//   const collection = db.collection('dishes');
+//   collection.insertOne(
+//     { name: 'pasta', description: 'Cheese Pasta' },
+//     (err, result) => {
+//       assert.equal(err, null);
+
+//       console.log('After Insert:\n');
+//       console.log(result.ops);
+
+//       collection.find({}).toArray((err, docs) => {
+//         assert.equal(err, null);
+
+//         console.log('Found:\n');
+//         console.log(docs);
+
+//         db.dropCollection('dishes', (err, result) => {
+//           assert.equal(err, null);
+//         });
+//       });
+//     }
+//   );
+
+//   // Running operations of other file
+//   dboper.insertDocument(
+//     db,
+//     { name: 'Pizza', description: 'Test' },
+//     'dishes',
+//     (result) => {
+//       console.log('Insert Document:\n', result.ops);
+
+//       dboper.findDocuments(db, 'dishes', (docs) => {
+//         console.log('Found Documents:\n', docs);
+
+//         dboper.updateDocument(
+//           db,
+//           { name: 'Pizza' },
+//           { description: 'Updated Test' },
+//           'dishes',
+//           (result) => {
+//             console.log('Updated Document:\n', result.result);
+
+//             dboper.findDocuments(db, 'dishes', (docs) => {
+//               console.log('Found Updated Documents:\n', docs);
+
+//               db.dropCollection('dishes', (result) => {
+//                 console.log('Dropped Collection: ', result);
+
+//                 client.close();
+//               });
+//             });
+//           }
+//         );
+//       });
+//     }
+//   );
+// });
