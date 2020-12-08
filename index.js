e6bb66da6045045a7e7f427e5ee8955359b37658
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-08 05:31:53 +0000

    Additional operations

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
  */
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 require('dotenv').config();
 
 // Update env to localhost  or rmove env file f running locally i.e outside devcontainer
@@ -36,9 +37,41 @@ MongoClient.connect(url, (err, client) => {
 
         db.dropCollection('dishes', (err, result) => {
           assert.equal(err, null);
-
-          client.close();
         });
+      });
+    }
+  );
+
+  // Running operations of other file
+  dboper.insertDocument(
+    db,
+    { name: 'Pizza', description: 'Test' },
+    'dishes',
+    (result) => {
+      console.log('Insert Document:\n', result.ops);
+
+      dboper.findDocuments(db, 'dishes', (docs) => {
+        console.log('Found Documents:\n', docs);
+
+        dboper.updateDocument(
+          db,
+          { name: 'Pizza' },
+          { description: 'Updated Test' },
+          'dishes',
+          (result) => {
+            console.log('Updated Document:\n', result.result);
+
+            dboper.findDocuments(db, 'dishes', (docs) => {
+              console.log('Found Updated Documents:\n', docs);
+
+              db.dropCollection('dishes', (result) => {
+                console.log('Dropped Collection: ', result);
+
+                client.close();
+              });
+            });
+          }
+        );
       });
     }
   );
---
+++ operations.js
@@ -0,0 +1,48 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-08 05:28:08
+ * @modify date 2020-12-08 05:28:08
+ * @desc Mongo operations
+ */
+const assert = require('assert');
+
+exports.insertDocument = (db, document, collection, callback) => {
+  const coll = db.collection(collection);
+  coll.insert(document, (err, result) => {
+    assert.equal(err, null);
+    console.log(
+      'Inserted ' +
+        result.result.n +
+        ' documents into the collection ' +
+        collection
+    );
+    callback(result);
+  });
+};
+
+exports.findDocuments = (db, collection, callback) => {
+  const coll = db.collection(collection);
+  coll.find({}).toArray((err, docs) => {
+    assert.equal(err, null);
+    callback(docs);
+  });
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+  const coll = db.collection(collection);
+  coll.deleteOne(document, (err, result) => {
+    assert.equal(err, null);
+    console.log('Removed the document ', document);
+    callback(result);
+  });
+};
+
+exports.updateDocument = (db, document, update, collection, callback) => {
+  const coll = db.collection(collection);
+  coll.updateOne(document, { $set: update }, null, (err, result) => {
+    assert.equal(err, null);
+    console.log('Updated the document with ', update);
+    callback(result);
+  });
+};
